a1eb6c23ac2718d7ed2ac44db2d5d160ec77db7d
LukAI <lukas.aistudia@gmail.com>
2025-12-18 23:04:56 +0100

    Delete verai.py

--- verai.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-API_KEY = "your_api_key"
-genai.configure(api_key=API_KEY)
-
-print("Dostępne modele:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"- {m.name}")
